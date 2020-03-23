6876c15c5fd566fb25a7c9bcf02b76a160521bd5
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2020-03-23 14:31:18 -0400

    Add complex type for output

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,22 @@
 output "application_queue_url" {
   description = "The URL for the created Amazon SQS queue for application"
-  value = module.application_queue.this_sqs_queue_id
+  value       = module.application_queue.this_sqs_queue_id
 }
 
 output "application_dlq_url" {
   description = "The URL for the created Amazon SQS dead leatter queue"
-  value = module.application_dlq.this_sqs_queue_id
+  value       = module.application_dlq.this_sqs_queue_id
+}
+
+output "queue_urls" {
+  value = [
+    {
+      url = module.application_queue.this_sqs_queue_id
+      dlq = false
+    },
+    {
+      url = module.application_dlq.this_sqs_queue_id
+      dlq = true
+    }
+  ]
 }
